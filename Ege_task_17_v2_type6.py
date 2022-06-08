a0284463ea31c1f9ef3ecdfbcdcff27bd3073eb5
artemiy-228 <79500772+artemiy-228@users.noreply.github.com>
2022-06-08 20:16:53 +0500

    Create Ege_task_17_v2_type6.py

---
+++ Ege_task_17_v2_type6.py
@@ -0,0 +1,30 @@
+#RUS - Найти количество пар, сумма модулей которых попадает в указанный промежуток. И найти максимальное число среди этих пар.
+#ENG - Fnd count pairs, which modules summ in interval [1500;4300]. ANd found bigger element on this pairs
+
+
+f = open("EGE_17_02.txt")
+list = []
+for i in f:
+    list.append(int(i))
+
+
+k = 0
+max = list[0]
+
+
+for i in range(len(list) - 1):
+    a = list[i]
+    b = list[i + 1]
+    if 1500 <= abs(a) + abs(b) <= 4300:
+        k += 1
+        if a > b and a > max:
+            max = a
+        elif b > a and b > max:
+            max = b
+
+
+
+
+print(k, max)
+
+
