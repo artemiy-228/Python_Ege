40c270779ea876e834d9037c9dba25da2b408a06
artemiy-228 <79500772+artemiy-228@users.noreply.github.com>
2022-06-18 12:36:23 +0500

    Update Ege_task_24_type16.py

--- Ege_task_24_type16.py
+++ Ege_task_24_type16.py
@@ -1,7 +1,7 @@
 #RUS - найдите количество букв рядом с которыми есть буква R
 #ENG - find count letters nearby letter R
 
-f = open("EGE_24_05_14.txt")
+f = open("EGE_24_05_20.txt")
 
 
 line = f.readline()
@@ -10,5 +10,9 @@ for i in range(1, len(line) - 1):
     if line[i-1] == "R" or line[i+1] == "R":
         k += 1
 
-print(k)
+if line[0:2] == "RR":
+    k += 1
+if line[-2:] == "RR":
+    k += 1
 
+print(k)
